57901ac13a0b69691eee3a59d34734c8096bd5ad
Jerry Vu <ufwjerryvu@gmail.com>
2025-02-07 23:48:10 +1100

    feat: setup user model and authentication

---
+++ backend/authentication/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        
+        return user
+    
