95fc05f4c5058557fa2bc551314957ae3fd1a5f6
Jerry Vu <ufwjerryvu@gmail.com>
2025-04-05 22:55:59 +1100

    feat: endpoints for group CRUD

--- backend/checklists/serializers.py
+++ backend/checklists/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import *
 
-class GroupSerializer(serializers.ModelSerializer):
+class GroupGeneralSerializer(serializers.ModelSerializer):
     """
     Serializer for group. Converts back and forth to and from native Python/Dja-
     ngo models/datatypes and JSON formats.
@@ -10,7 +10,15 @@ class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = ["id", "creator_id", "name", "description"]
-        read_only_fields = ["id"]
+        fields = ["id", "creator", "name", "description"]
+        read_only_fields = ["id", "creator"]
 
-    
+class GroupCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for creating a group (only for creating a group). This is because
+    we want the creator to be writeable when creating.
+    """
+    class Meta:
+        model = Group
+        fields = ["id", "creator", "name", "description"]
+        read_only_fields = ["id"]
